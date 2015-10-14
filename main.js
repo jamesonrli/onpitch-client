29cf3044a5ddec96c1123e56d57eec3cf7b68318
Jameson Li <jamesonrli@gmail.com>
2015-10-14 00:59:24 -0700

    hello world

---
+++ main.js
@@ -0,0 +1,16 @@
+// main.js
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var HelloWorld = React.createClass({
+  render: function() {
+    return (
+      <h1>Hello, world!</h1>
+    );
+  }
+});
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('container')
+);
