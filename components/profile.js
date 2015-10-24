f3885255226cc3922a1a6b18e8911c9b1d79d258
Jameson Li <jamesonrli@gmail.com>
2015-10-24 00:30:08 -0700

    profile projects

--- components/profile.js
+++ components/profile.js
@@ -19,6 +19,8 @@ var ProfileBox = React.createClass({
     return {
       user: new UserData("John Smith", "This is my awesome profile", "/url/to/profile/image"),
       projects: [
+        new ProjectData("My Awesome Project", "This is amazing project description", "/url/to/project/image"),
+        new ProjectData("My Awesome Project", "This is amazing project description", "/url/to/project/image"),
         new ProjectData("My Awesome Project", "This is amazing project description", "/url/to/project/image")
       ]
     }
@@ -51,9 +53,19 @@ var ProfileIntro = React.createClass({
 var ProfileProjects = React.createClass({
   render: function() {
     return (
-      <div>
-        {this.props.projects.map(function() {
-
+      <div className='profileProjectList container-fluid'>
+        {this.props.projects.map(function(project, i) {
+          return (
+            <div key={i} className='profileProjectBlock container col-sm-6'>
+              <div className="col-xs-4">
+                <img className='profileProjectImage thumbnail' style={{width:'100px'},{height:'100px'}} src='http://icons.iconarchive.com/icons/hopstarter/3d-cartoon-vol3/256/Internet-Explorer-icon.png' />
+              </div>
+              <div className='profileProjectDetails col-xs-8'>
+                <p className="list-group-item-heading">{project.name}</p>
+                <p className="list-group-item-text">{project.description}</p>
+              </div>
+            </div>
+          );
         })}
       </div>
     );
