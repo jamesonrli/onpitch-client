f7dbc3033722499fd065087c6ae029db913248e3
Peter Tran <tranpeter93@gmail.com>
2015-11-04 22:07:00 -0800

    Fixed Google Identity Sign In
    gapi should now properly try to sign user in when clicking on the "log
    in" button

--- app/components/login.js
+++ app/components/login.js
@@ -5,29 +5,25 @@ var React = require('react');
 var MainActions = require('../actions/main_actions');
 var OnPitchConstants = require('../common/constants');
 
-var Login = React.createClass({
+// Automatically signs user through google. 
+	// May need to change to prompt each time.
+var gSignIn = function() {
+	var googleUser = gapi.auth2.getAuthInstance();
+	if (googleUser && !googleUser.isSignedIn.get()) googleUser.signIn();
+}
 
+var Login = React.createClass({	
 	// Loads needed APIs
 	googleAuth: function () {
 		gapi.load("auth2", function() {
-			gapi.auth2.init({"client_id" : gapi_clientId, "cookie_policy": "none"});
-		});
-		
-		gapi.load('client', function() {
-			gapi.client.load('plus', 'v1');
+			gapi.auth2.init({"client_id" : gapi_clientId, "cookie_policy": "none"}).then(gSignIn);
 		});				
-	},
-
-	// Automatically signs user through google. 
-	// May need to change to prompt each time.
-	googleSignIn: function() {
-		var googleUser = gapi.auth2.getAuthInstance();
-		if (googleUser && !googleUser.isSignedIn.get()) googleUser.signIn();
 	},
 		
 	//Signs current user out
 	signOut: function() {
-		gapi.auth2.getAuthInstance().signOut();
+		var opts = {'prompt':'login'};
+		gapi.auth2.getAuthInstance().signOut(opts);
 	},
 	
 	// Pre-req: successfull googleAuth
