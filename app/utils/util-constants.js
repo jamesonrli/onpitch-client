989c885c03231206c8a499b67f48b32eb950e666
Jameson Li <jamesonrli@gmail.com>
2015-11-06 20:10:18 -0800

    fix port

--- app/utils/util-constants.js
+++ app/utils/util-constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   INTERNAL_HOST: process.env.NODE_ENV == 'production' ? 'onpitch.herokuapp.com' : 'localhost',
-  INTERNAL_PORT: process.env.PORT_SERVER || 5050,
+  INTERNAL_PORT: process.env.PORT || 5050,
 
   GET: 'GET',
   PUT: 'PUT'
