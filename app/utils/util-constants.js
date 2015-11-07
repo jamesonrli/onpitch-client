a1d8ad699d37f2dc0e4e7d70981ce0787686e8f4
Jameson Li <jamesonrli@gmail.com>
2015-11-06 20:21:32 -0800

    maybe fix?

--- app/utils/util-constants.js
+++ app/utils/util-constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-  INTERNAL_HOST: process.env.NODE_ENV == 'production' ? 'onpitch.herokuapp.com' : 'localhost',
+  INTERNAL_HOST: (process.env.NODE_ENV == 'production') ? 'onpitch.herokuapp.com' : 'localhost',
   INTERNAL_PORT: process.env.PORT || 5050,
 
   GET: 'GET',
